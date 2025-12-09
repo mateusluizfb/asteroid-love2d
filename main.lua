7f3d8e4ccf9e23481cf544ee615369c286103319
Mateus Luiz <mateuslfreitasb@gmail.com>
2025-12-09 20:40:40 -0300

    feat: Make the ship move

--- main.lua
+++ main.lua
@@ -6,54 +6,41 @@ function love.load()
   SpaceShip = {
     x = windowCenterX,
     y = windowCenterY,
-    width = 15,
-    height = 30,
-    angle = 0
+    width = 30,
+    height = 15,
+    angle = 0,
+    speed = 20,
+    turnSpeed = 2,
   }
 end
 
-function updateSpaceship(spaceShip)
-  local x, y = love.mouse.getPosition()
+function updateSpaceship(dt, spaceShip)
+  local moveAmount = spaceShip.speed * dt
 
-  print("mouse x: " .. x )
-  print("spaceship x: " .. spaceShip.x)
+  if love.keyboard.isDown("up") then
+    spaceShip.x = spaceShip.x + math.cos(spaceShip.angle) * moveAmount
+    spaceShip.y = spaceShip.y + math.sin(spaceShip.angle) * moveAmount
+  end
 
-  local correctionOffset = math.pi / 2
-  spaceShip.angle = math.atan2(y - spaceShip.y, x - spaceShip.x) + correctionOffset
+  if love.keyboard.isDown("down") then
+    spaceShip.x = spaceShip.x - math.cos(spaceShip.angle) * moveAmount
+    spaceShip.y = spaceShip.y - math.sin(spaceShip.angle) * moveAmount
+  end
+
+  local turn = spaceShip.turnSpeed * dt
+
+  if love.keyboard.isDown("left") then 
+    spaceShip.angle = spaceShip.angle - turn
+  end
+
+  if love.keyboard.isDown("right") then
+    spaceShip.angle = spaceShip.angle + turn
+  end
 end
 
 function love.update(dt)
-  updateSpaceship(SpaceShip)
+ updateSpaceship(dt, SpaceShip)
 end
-
-function drawSpaceshipMath(spaceShip)
-  local width = spaceShip.width
-  local height = spaceShip.height
-  local angle = spaceShip.angle
-
-  local tip_x, tip_y = 0, -height / 2
-  local left_x, left_y = -width / 2, height / 2
-  local right_x, right_y = width / 2, height / 2
-
-  local cosA = math.cos(angle)
-  local sinA = math.sin(angle)
-
-  local tip_rx = tip_x * cosA - tip_y * sinA + spaceShip.x
-  local tip_ry = tip_x * sinA + tip_y * cosA + spaceShip.y
-
-  local left_rx = left_x * cosA - left_y * sinA + spaceShip.x
-  local left_ry = left_x * sinA + left_y * cosA + spaceShip.y
-
-  local right_rx = right_x * cosA - right_y * sinA + spaceShip.x
-  local right_ry = right_x * sinA + right_y * cosA + spaceShip.y
-
-  love.graphics.polygon("fill",
-    tip_rx, tip_ry,
-    left_rx, left_ry,
-    right_rx, right_ry
-  )
-end
-
 
 function drawSpaceshipLove(spaceShip)
   local width = spaceShip.width
@@ -65,9 +52,9 @@ function drawSpaceshipLove(spaceShip)
   love.graphics.translate(spaceShip.x, spaceShip.y)
   love.graphics.rotate(spaceShip.angle)
 
-  local px1, py1 = 0, -height / 2
+  local px1, py1 = width / 2, 0
   local px2, py2 = -width / 2, height / 2
-  local px3, py3 = width / 2, height / 2
+  local px3, py3 = -width / 2, -height / 2
 
   love.graphics.polygon("fill",
     px1, py1,
